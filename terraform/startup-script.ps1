c2833d16c27cf60bae7bc7116ec0b9a33abdfb4c
kpoxo6op <kpoxo6op@gmail.com>
2024-04-27 22:06:27 +1200

    install-docker-ce.ps1

--- terraform/startup-script.ps1
+++ terraform/startup-script.ps1
@@ -3,6 +3,9 @@ $dir = Split-Path -Parent $logFile
 New-Item -ItemType Directory -Path $dir
 Start-Transcript -Path $logFile -Append
 
-Write-Output "Create GitLab runner"
+Write-Output "Install Docker"
+$ScriptUrl = "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1"
+Invoke-WebRequest -Uri $ScriptUrl -OutFile "install-docker-ce.ps1"
+.\install-docker-ce.ps1
 
 Stop-Transcript
