9423f8d7efdca35799a87789b4554e4f1ebaeae9
kpoxo6op <kpoxo6op@gmail.com>
2024-04-28 21:19:07 +1200

    add non-working & .\gitlab-runner.exe @args

--- terraform/register-runner.ps1
+++ terraform/register-runner.ps1
@@ -8,12 +8,12 @@ Set-Location C:\GitLab-Runner
 
 Invoke-WebRequest -Uri "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile gitlab-runner.exe
 Write-Output "Regsiter runner with token ${runner_token}"
-
+# todo fix syntax
 $args = @(
     "register",
     "--non-interactive",
     "--url", "https://gitlab.com/",
-    "--token", ${runner_token},
+    "--token", "${runner_token}",
     "--executor", "docker",
     "--docker-image", "alpine:latest",
     "--builds-dir", "C:\GitLab-Runner",
