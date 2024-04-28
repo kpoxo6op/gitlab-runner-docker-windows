a5f229211b02da326708a7fb609f537db59e2b9d
kpoxo6op <kpoxo6op@gmail.com>
2024-04-28 20:53:58 +1200

    init & .\gitlab-runner.exe @args

--- terraform/register-runner.ps1
+++ terraform/register-runner.ps1
@@ -4,9 +4,22 @@ New-Item -ItemType Directory -Path $dir
 Start-Transcript -Path $logFile -Append
 
 New-Item -Path C:\GitLab-Runner -ItemType Directory
+Set-Location C:\GitLab-Runner
 
-Invoke-WebRequest -Uri "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile C:\GitLab-Runner\gitlab-runner.exe
-
+Invoke-WebRequest -Uri "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile gitlab-runner.exe
 Write-Output "Regsiter runner with token ${runner_token}"
+
+$args = @(
+    "register",
+    "--non-interactive",
+    "--url", "https://gitlab.com/",
+    "--token", ${runner_token},
+    "--executor", "docker",
+    "--docker-image", "alpine:latest",
+    "--builds-dir", "C:\GitLab-Runner",
+    "--cache-dir", "C:\GitLab-Runner",
+    "--description", "docker runner"
+)
+& .\gitlab-runner.exe @args
 
 Stop-Transcript
