88db3245fb8c6734a1609c26b77de07a6825239d
kpoxo6op <kpoxo6op@gmail.com>
2024-04-30 21:28:20 +1200

    save progress

--- terraform/register-runner.ps1
+++ terraform/register-runner.ps1
@@ -7,19 +7,41 @@ New-Item -Path C:\GitLab-Runner -ItemType Directory
 Set-Location C:\GitLab-Runner
 
 Invoke-WebRequest -Uri "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile gitlab-runner.exe
-Write-Output "Regsiter runner with token ${runner_token}"
+Write-Output "Register runner with token ${runner_token}"
 # todo fix syntax
-$args = @(
+$registerParams = @(
     "register",
-    "--non-interactive",
-    "--url", "https://gitlab.com/",
-    "--token", "${runner_token}",
-    "--executor", "docker",
-    "--docker-image", "alpine:latest",
     "--builds-dir", "C:\GitLab-Runner",
     "--cache-dir", "C:\GitLab-Runner",
-    "--description", "docker runner"
+    "--config", "C:\GitLab-Runner\config.toml",
+    "--description", "docker runner",
+    "--docker-image", "alpine:latest",
+    "--executor", "docker",
+    "--non-interactive",
+    "--token", "${runner_token}",
+    "--url", "https://gitlab.com/"
 )
-& .\gitlab-runner.exe @args
+& .\gitlab-runner.exe @registerParams
+
+Write-Output "Install runner service"
+$commonParams = @{
+    FilePath    = C:\GitLab-Runner\gitlab-runner.exe
+    NoNewWindow = $true
+    Wait        = $true
+}
+$installArgs = @(
+  "install",
+  "--working-directory", "C:\GitLab-Runner",
+  "--config", "C:\GitLab-Runner\config.toml"
+)
+Start-Process @commonParams -ArgumentList $installArgs
+
+Write-Output "Start runner service"
+Start-Process @commonParams -ArgumentList "start"
+
+Write-Output "Verify runners"
+& .\gitlab-runner.exe "verify"
+
+Get-WinEvent -ProviderName gitlab-runner | Format-Table -wrap -auto
 
 Stop-Transcript
