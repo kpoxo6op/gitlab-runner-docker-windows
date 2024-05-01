9de2a1fae051aea00883fa83a109e225bb527677
kpoxo6op <kpoxo6op@gmail.com>
2024-05-01 21:58:51 +1200

    add gitlab ci

--- terraform/gitlab.tf
+++ terraform/gitlab.tf
@@ -1,12 +1,21 @@
 resource "gitlab_project" "project" {
-  name = "sample project"
-  description = "gcp pwsh runner"
+  name             = "sample project"
+  description      = "gcp pwsh runner"
   visibility_level = "public"
 }
 
 resource "gitlab_user_runner" "runner" {
   runner_type = "project_type"
-  project_id = gitlab_project.project.id
+  project_id  = gitlab_project.project.id
   description = "build project on gcp vm in docker"
-  tag_list = ["windows","docker"]
+  tag_list    = ["windows", "docker"]
+}
+
+resource "gitlab_repository_file" "pipeline" {
+  project        = gitlab_project.project.id
+  file_path      = ".gitlab-ci.yml"
+  branch         = "main"
+  content        = base64encode(file("${path.module}/.gitlab-ci.yml"))
+  commit_message = "init docker for windows pipeline"
+  author_name    = "Terraform"
 }
